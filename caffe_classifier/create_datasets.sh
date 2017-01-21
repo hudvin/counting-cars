fbdb254cd03000792bfbb6e4c185c0c21267a7e9
hudvin <hudvin@gmail.com>
2017-01-21 16:22:08 +0200

    add caffe classifier

---
+++ caffe_classifier/create_datasets.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/opt/caffe/build/tools/compute_image_mean dataset/train_lmdb mean_image.binaryproto
---
+++ caffe_classifier/train_ordinary.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/opt/caffe/build/tools/caffe train --solver models/ordinary/solver_1.prototxt
